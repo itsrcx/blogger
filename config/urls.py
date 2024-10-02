2beb9147fd8c0fc59c006078dd150d19dc66cbf9
joyboy <ramanchauhanxx@gmail.com>
2024-10-03 01:25:26 +0530

    cmd correction

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # function for aws health check as root has no webpage to serve
 def hellow(request):
-    return render(request, 'hello.html')
+    return HttpResponse("OK")
 
 urlpatterns = [
     path('', hellow),
