bc7bf58375ba3cc1096767da5286b50191ad5c7d
joyboy <ramanchauhanxx@gmail.com>
2024-10-02 23:58:28 +0530

    requirement changes as python 3.8 require django-4.2

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-sz*6#)t&*nn5#(u^y2$w$y-=gc8r1csdz=b8r#lx6w@=@)o!c6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dev-portfolio-env.eba-yujigmmp.ap-south-1.elasticbeanstalk.com',]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
 
     # 3rd Party
     'rest_framework',
-    'ebhealthcheck.apps.EBHealthCheckConfig',
 
     # local apps
     'blog',
